c335da2d1aa171165a7115e4d47be244675d770c
kevin zhou <kevinzhou2@live.cn>
2022-04-23 00:09:32 -0700

    .

--- start.sh
+++ start.sh
@@ -9,5 +9,5 @@ mkdir log
 mkdir model
 rm -f ./log/*.txt
 rm -f ./tensorboard/*
-python main.py --train_num_points 100000 --valid_num_points 2000 --test_iter 5000 --rep_iter 1000 --batch_size 32 --exp_name servertest
+python main.py --train_num_points 100000 --valid_num_points 2000 --test_iter 5000 --rep_iter 1000 --batch_size 40 --exp_name servertest
 
