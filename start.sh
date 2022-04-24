c30c129d894dd2c90f8b66ab8724bebb842e7575
kevin zhou <kevinzhou2@live.cn>
2022-04-23 19:26:21 -0700

    .

--- start.sh
+++ start.sh
@@ -11,6 +11,6 @@ rm -f ./log/*.txt
 rm -f ./tensorboard/*
 python main.py --train_num_points 100000 \
     --valid_num_points 2000 --test_iter 25000 \
-    --rep_iter 2000 --batch_size 48 \
+    --rep_iter 2000 --batch_size 40 \
     --exp_name servertest --D_pretrain_iter 5000 --lambda_once 0
 
