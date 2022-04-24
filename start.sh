b6c39b361c6d012071bc3b5ac5f1d7e342173add
kevin zhou <kevinzhou2@live.cn>
2022-04-24 11:20:37 -0700

    .

--- start.sh
+++ start.sh
@@ -12,5 +12,5 @@ rm -f ./tensorboard/*
 python main.py --train_num_points 100000 \
     --valid_num_points 2000 --test_iter 25000 \
     --rep_iter 2000 --batch_size 44 \
-    --exp_name onlyCycleloss --D_pretrain_iter 5000 --lambda_once 0 --lambda_B 1 --lambda_A 1 
+    --exp_name nocycleloss --D_pretrain_iter 5000 --lambda_once 1 --lambda_B 0 --lambda_A 0
 
