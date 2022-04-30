fb040233fa5571b13123c4128a55d6bf130642de
kevin zhou <kevinzhou2@live.cn>
2022-04-30 09:45:35 -0700

    .

--- start.sh
+++ start.sh
@@ -13,6 +13,6 @@ rm -f ./tensorboard/*
 python main.py --num_workers 2 --train_num_points 50000 \
     --valid_num_points 1000 --test_iter 10000 \
     --rep_iter 1000 --batch_size 40 \
-    --exp_name decayLR --D_pretrain_iter 10000 --lambda_once 1 --lambda_B 0 --lambda_A 0\
-    --load_D 0 --valid_begin 1 
+    --exp_name trainD --D_pretrain_iter 10000 --lambda_once 1 --lambda_B 0 --lambda_A 0\
+    --load_D 0 --valid_begin 1 --train_D 1
 
