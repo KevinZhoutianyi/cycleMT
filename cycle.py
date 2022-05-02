fcd0c5c9d5e2274d3cff3a0d3d250f3b0164535e
kevin zhou <kevinzhou2@live.cn>
2022-05-02 01:17:31 -0700

    .

--- cycle.py
+++ cycle.py
@@ -131,10 +131,10 @@ class CycleGAN():
 
         # GAN loss D_A(G_A(A))
         # self.loss_G_A = self.criterionGAN(self.D_A(self.fake_B,self.fake_B_attn), torch.ones((self.fake_B.shape[0],1),device=self.device))*lambda_once
-        self.loss_G_A = -self.D_A(self.fake_B,self.fake_B_attn)*lambda_once
+        self.loss_G_A = torch.mean(-self.D_A(self.fake_B,self.fake_B_attn))*lambda_once
         # GAN loss D_B(G_B(B))
         # self.loss_G_B = self.criterionGAN(self.D_B(self.fake_A,self.fake_A_attn), torch.ones((self.fake_A.shape[0],1),device=self.device))*lambda_once
-        self.loss_G_B = -self.D_B(self.fake_A,self.fake_A_attn)*lambda_once
+        self.loss_G_B = torch.mean(-self.D_B(self.fake_A,self.fake_A_attn))*lambda_once
 
 
         # Forward cycle loss || G_B(G_A(A)) - A||
