655f72b79a3a732b2aaa1fb79c3ff6cfbf12ed79
kevin zhou <kevinzhou2@live.cn>
2022-05-02 16:30:40 -0700

    .

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ python main.py  --num_workers 4             --batch_size 32             --max_le
                 --rep_iter 1000             --D_pretrain_iter 5000      --test_iter 5000    \
                 --lambda_once 1             --lambda_B 100                --lambda_A 100        \
                 --load_D 0                  --load_G 0                  --valid_begin 1             --train_D 1         \
-                --exp_name  fixnoisebug
+                --exp_name  cycleloss
