e44dcdbb070e3faf12a812e48418c50ec98f4470
kevin zhou <kevinzhou2@live.cn>
2022-05-05 01:22:13 -0700

    .

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ python main.py  --num_workers 4             --batch_size 32             --max_le
                 --rep_iter 1000             --D_pretrain_iter 5000      --test_iter 10000    \
                 --lambda_once 1             --lambda_B 25                --lambda_A 25         --lambda_GP 0.1    \
                 --load_D 0                  --load_G 0                  --valid_begin 1             --train_D 1         \
-                --exp_name baseline
+                --exp_name 0.1GP
