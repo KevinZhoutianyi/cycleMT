ad6b306767c687466a5289016d53b6166f66b1b8
kevin zhou <kevinzhou2@live.cn>
2022-05-10 22:23:04 -0700

    .

--- start.sh
+++ start.sh
@@ -12,8 +12,8 @@ rm -f ./log/*.txt
 rm -f ./tensorboard/* 
 python main.py  --num_workers 4             --batch_size 32             --max_length 256    \
                 --train_num_points 60000   --valid_num_points 2195     --poolsize 1                    \
-                --D_lr 5e-5                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
+                --D_lr 1e-4                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
                 --rep_iter 1000             --D_pretrain_iter 3000         --test_iter 10000    \
-                --lambda_once 5             --lambda_B 0                --lambda_A 0         --lambda_GP 1    \
+                --lambda_once 10             --lambda_B 50                --lambda_A 50         --lambda_GP 1    \
                 --load_D 0                  --load_G 0                  --valid_begin 1             --train_D 1         \
                 --exp_name domaintransferbible,nocycle
