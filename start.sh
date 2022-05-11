85aeb9c567192abd5a5539c34c84df1fa85696ba
kevin zhou <kevinzhou2@live.cn>
2022-05-11 11:12:16 -0700

    .

--- start.sh
+++ start.sh
@@ -8,12 +8,13 @@ pip install wandb
 mkdir log
 mkdir model
 mkdir checkpoint
+rm -f ./checkpoint/*
 rm -f ./log/*.txt
 rm -f ./tensorboard/* 
 python main.py  --num_workers 4             --batch_size 32             --max_length 256    \
                 --train_num_points 60000   --valid_num_points 2195     --poolsize 1                    \
                 --D_lr 5e-5                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
-                --rep_iter 1000             --D_pretrain_iter 3000         --test_iter 10000    \
-                --lambda_once 0             --lambda_B 50                --lambda_A 50         --lambda_GP 1    \
+                --rep_iter 1000             --D_pretrain_iter 120000         --test_iter 10000    \
+                --lambda_once 1             --lambda_B 20                --lambda_A 20         --lambda_GP 1    \
                 --load_D 0                  --load_G 0                  --valid_begin 1             --train_D 1         \
-                --exp_name domaintransferbible,onlycycle
+                --exp_name bible,onlytrainD
