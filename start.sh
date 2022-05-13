5b8963acff6a0ed10a9d11664c93727971f8924d
kevin zhou <kevinzhou2@live.cn>
2022-05-13 12:29:24 -0700

    .

--- start.sh
+++ start.sh
@@ -11,10 +11,10 @@ mkdir checkpoint
 rm -f ./checkpoint/*
 rm -f ./log/*.txt
 rm -f ./tensorboard/* 
-python main.py  --num_workers 4             --batch_size 36             --max_length 256    \
+python main.py  --num_workers 4             --batch_size 32             --max_length 256    \
                 --train_num_points 60000   --valid_num_points 2195     --poolsize 1                    \
                 --D_lr 5e-5                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
                 --rep_iter 1000             --D_pretrain_iter 0         --test_iter 10000    \
-                --lambda_once 1             --lambda_B 10                --lambda_A 10         --lambda_GP 1    \
+                --lambda_once 1             --lambda_B 0                --lambda_A 0         --lambda_GP 1    \
                 --load_D 1                  --load_G 0                  --valid_begin 1             --train_D 0         \
-                --exp_name bible,LoadD,OnceandCycle
+                --exp_name bible,LoadD,onlyOnce
