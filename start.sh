f5b64bcce57a02b171d8430595e317eddfe0041a
kevin zhou <kevinzhou2@live.cn>
2022-05-13 11:13:57 -0700

    .

--- start.sh
+++ start.sh
@@ -17,4 +17,4 @@ python main.py  --num_workers 4             --batch_size 44             --max_le
                 --rep_iter 1000             --D_pretrain_iter 0         --test_iter 10000    \
                 --lambda_once 1             --lambda_B 10                --lambda_A 10         --lambda_GP 1    \
                 --load_D 1                  --load_G 0                  --valid_begin 1             --train_D 0         \
-                --exp_name bible,LoadD,onlyOnce
+                --exp_name bible,LoadD,onceandcycle
