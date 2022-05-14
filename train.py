fea7f525ae4043f5b04bb047dedf3fba55df6ab6
kevin zhou <kevinzhou2@live.cn>
2022-05-13 17:15:03 -0700

    .

--- train.py
+++ train.py
@@ -20,9 +20,13 @@ def my_train(loader,model,total_iter,args,logging,valid_loader,tokenizer,wandb):
         if(total_iter[0]<args.D_pretrain_iter):
             model.optimize_parameters(trainD=True,trainG=False)
         else:
-            if(torch.rand(1)<0.2):
+            p = torch.rand(1)
+            print(p)
+            if(p<0.2):
+                print("!")
                 model.optimize_parameters(trainD=args.train_D,trainG=True)
             else:
+                print("?")
                 model.optimize_parameters(trainD=args.train_D,trainG=False)
 
         # nn.utils.clip_grad_norm(model.D_A.parameters(), args.D_grad_clip)
