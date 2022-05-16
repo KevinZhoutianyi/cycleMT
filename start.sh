298b5bb3daf5cabdc731a83ccfda0dbe9738f23c
kevin zhou <kevinzhou2@live.cn>
2022-05-16 10:09:50 -0700

    .

--- start.sh
+++ start.sh
@@ -13,8 +13,8 @@ rm -f ./log/*.txt
 rm -f ./tensorboard/* 
 python main.py  --num_workers 4             --batch_size 36             --max_length 256    \
                 --train_num_points 200000   --valid_num_points 3500     --poolsize 1                    \
-                --D_lr 5e-5                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
+                --D_lr 5e-6                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
                 --rep_iter 1000             --D_pretrain_iter 5000         --test_iter 10000    \
                 --lambda_once 1             --lambda_B 0                --lambda_A 0        --lambda_GP 50    \
-                --load_D 0                  --load_G 0                  --valid_begin 1             --train_D 1       --DperG 5  \
-                --exp_name bible,50newGP,onlyOnce,DperG5
+                --load_D 0                  --load_G 0                  --valid_begin 1             --train_D 1       --DperG 15  \
+                --exp_name 50newGP,onlyOnce,DperG15,dlr5e-6
