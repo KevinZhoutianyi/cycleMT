68a3d2243e36254a5df0482de5d023b354b944fa
kevin zhou <kevinzhou2@live.cn>
2022-05-16 01:23:07 -0700

    .

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ mkdir checkpoint
 rm -f ./checkpoint/*
 rm -f ./log/*.txt
 rm -f ./tensorboard/* 
-python main.py  --num_workers 4             --batch_size 32             --max_length 256    \
+python main.py  --num_workers 4             --batch_size 36             --max_length 256    \
                 --train_num_points 60000   --valid_num_points 2195     --poolsize 1                    \
                 --D_lr 5e-5                 --G_lr 5e-6                 --D_gamma 1       --G_gamma 1      \
                 --rep_iter 1000             --D_pretrain_iter 2000         --test_iter 10000    \
