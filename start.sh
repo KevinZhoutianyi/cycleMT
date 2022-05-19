ed90cf8aee691b526688fa96e9780dc30ca71e70
kevin zhou <kevinzhou2@live.cn>
2022-05-19 12:04:59 -0700

    .

--- start.sh
+++ start.sh
@@ -15,4 +15,4 @@ python main.py --num_workers 4 --batch_size 32 --max_length 256 --train_num_poin
                 --valid_num_points 3500 --poolsize 1 --D_lr 5e-6 --G_lr 5e-6 --D_gamma 1 --G_gamma 1 --rep_iter 1000 \
                 --D_pretrain_iter 5000 --test_iter 10000 --lambda_once 1 --lambda_B 20 --lambda_A 20 --lambda_GP 50 \
                 --load_D 0 --load_G 0 --valid_begin 1 --train_D 1 --DperG 15\
-                --exp_name 1once.20cycle,800kdata,logitcycle
+                --exp_name 1once.20cycle,800kdata.baseline
