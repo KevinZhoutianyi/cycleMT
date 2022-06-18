6e02e03ed0c08cbc27304621fa65238f7ef3e75e
kevin zhou <kevinzhou2@live.cn>
2022-06-17 22:27:16 -0700

    .

--- pretrain/start.sh
+++ pretrain/start.sh
@@ -1,1 +1,13 @@
+
+pip install torch_optimizer
+pip install transformers==4.18.0
+pip install datasets
+pip install sacrebleu==1.5.1
+pip install wandb
+mkdir log
+mkdir model
+mkdir checkpoint
+rm -f ./checkpoint/*
+rm -f ./log/*.txt
+rm -f ./tensorboard/* 
 python pretrain.py
